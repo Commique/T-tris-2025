337b81a256f47d85d146a765a500876f13b5da63
Commique <bouvet.come@gmail.com>
2025-01-16 21:21:01 +0100

    Bug freee

--- Tetris.py
+++ Tetris.py
@@ -11,45 +11,44 @@ main_window = pygame.display.set_mode()
 screen_resolution = list(main_window.get_size())
 good_screen_resolution = [1366, 745]
 main_window = pygame.display.set_mode((500, 500), RESIZABLE)
-"""pygame.display.toggle_fullscreen()"""
 pygame.display.set_caption("Tétris")
 pygame.display.set_icon(pygame.image.load("Tetris.jpg"))
 clock = pygame.time.Clock()
 last_update = pygame.time.get_ticks() + 2*vitesse
 running = True
 
+"""
+BUG : 
+-   Implémenter que la pièce baton se décale
+-   La fonction de reset ne se declenche pas lorsque que Key_r is pressed
+
+TODO :
+- Le jeu au centre
+- Le score
+- Les prochaines pièces 
+- Le meilleur score de la session
+- Tétris, écrit en gros ou une image *non* pixelisée
+- Changer couleur lettre + graphiques
+- Niveaux ! Vitesse !
+- Fenêtre de jeu game over
+- Fonction Reset 
+- Fenêtre paramètres de jeu : arrêt et replay 
+"""
+
 while game_on:
     #Variable qui contient les touches pressées
     keys = pygame.key.get_pressed()
-    
+
+    #Limiter les fps à 60
+    clock.tick(60)
+
+    #Tout ce qui a un rapport avec l'affichage
+    main_window.fill(color[1])
+    width, height = pygame.display.get_surface().get_size()
+    pixel = update_window(width, height)
+
     #Boucle du jeu
-    if running:
-        #Limiter les fps à 60
-        clock.tick(60)
-
-        #Tout ce qui a un rapport avec l'affichage
-        main_window.fill(color[1])
-        width, height = pygame.display.get_surface().get_size()
-        pixel = update_window(width, height)
-       
-        """
-        BUG : 
-        -   Implémenter que la pièce baton se décale
-        -   La fonction de reset ne se declenche pas lorsque que Key_r is pressed
-        
-        TODO :
-        - Le jeu au centre
-        - Le score
-        - Les prochaines pièces 
-        - Le meilleur score de la session
-        - Tétris, écrit en gros ou une image *non* pixelisée
-        - Changer couleur lettre + graphiques
-        - Niveaux ! Vitesse !
-        - Fenêtre de jeu game over
-        - Fonction Reset 
-        - Fenêtre paramètres de jeu : arrêt et replay 
-        """
-
+    if running:     
         #La pièce qui tombe toutes les vitesse/1000 seconde
         if pygame.time.get_ticks() >= last_update:
             bloc_bundle, grille, running, score_bundle = check_down_collision(bloc_bundle, grille, score_bundle)
@@ -80,6 +79,7 @@ while game_on:
                         main_window = pygame.display.set_mode((500, 500), RESIZABLE)
                     else:
                         main_window = pygame.display.set_mode()
+                        pygame.display.toggle_fullscreen()
 
                 #Mouvement
                 if event.key == pygame.K_DOWN:
@@ -94,34 +94,6 @@ while game_on:
         #Aller vite
         if keys[pygame.K_DOWN]:
             bloc_bundle, grille, running, score_bundle = check_down_collision(bloc_bundle, grille, score_bundle)
-
-        #Remplissage des couleurs dans la grille
-        #Variable pour centrer la grille
-        top_right_corner = [width/2 - 5*pixel, height/2 - 11*pixel]
-        if list(main_window.get_size())[0] == good_screen_resolution[0] and list(main_window.get_size())[1] == good_screen_resolution[1]:
-            pygame.draw.rect(main_window, color[0], pygame.Rect(top_right_corner[0] - int(1/2*pixel), top_right_corner[1] - int(1/2*pixel), 11*pixel, 23*pixel), int(1/2*pixel))
-        for y in range(len(grille)):
-            for x in range(len(grille[y])):
-                pygame.draw.rect(main_window, color[0], pygame.Rect(x*pixel + top_right_corner[0], y*pixel + top_right_corner[1], pixel, pixel), 1)
-                if grille[y][x] != 0:
-                    pygame.draw.rect(main_window, color[grille[y][x]], pygame.Rect(x*pixel + top_right_corner[0]+1, y*pixel + top_right_corner[1]+1, pixel-2, pixel-2))
-                    pygame.draw.rect(main_window, brighter_color[grille[y][x]], pygame.Rect(x*pixel + top_right_corner[0] + int(1/4*pixel), y*pixel + top_right_corner[1] + int(1/4*pixel), int(1/4*pixel), int(1/4*pixel)))
-        #Dessiner la pièce qui bouge
-        for y in range(len(bloc_bundle[0])):
-            for x in range(len(bloc_bundle[0][y])):
-                if bloc_bundle[0][y][x] != 0:
-                    pygame.draw.rect(main_window, color[bloc_bundle[0][y][x]], pygame.Rect(x*pixel + top_right_corner[0]+1 + pixel*bloc_bundle[1][1], y*pixel + top_right_corner[1]+1 + pixel*bloc_bundle[1][0], pixel-2, pixel-2))
-                    pygame.draw.rect(main_window, brighter_color[bloc_bundle[0][y][x]], pygame.Rect(x*pixel + top_right_corner[0] + int(1/4*pixel) + pixel*bloc_bundle[1][1], y*pixel + top_right_corner[1] + int(1/4*pixel) + pixel*bloc_bundle[1][0], int(1/4*pixel), int(1/4*pixel)))
-        #Dessiner la pièce qui va venir
-        #pygame.draw.rect(main_window, color[0], pygame.Rect(width/2 + 7*pixel, height/2, len(blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][0])*pixel, len(blocs[bloc_bundle[4][1][bloc_bundle[4][0]]])*pixel))
-        for y in range(len(blocs[bloc_bundle[4][1][bloc_bundle[4][0]]])):
-            for x in range(len(blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y])):
-                if blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x] != 0:
-                    pygame.draw.rect(main_window, color[0], pygame.Rect((x+7)*pixel + width/2, (y-7)*pixel + height/2, pixel, pixel), 2)
-                    pygame.draw.rect(main_window, color[blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x]], pygame.Rect((x+7)*pixel + width/2 + 1, (y-7)*pixel + height/2 + 1, pixel-2, pixel-2))
-                    pygame.draw.rect(main_window, brighter_color[blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x]], pygame.Rect((x+7)*pixel + width/2 + int(1/4*pixel), (y-7)*pixel + height/2 + int(1/4*pixel), int(1/4*pixel), int(1/4*pixel)))
-                """elif blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x] == 0:
-                    pygame.draw.rect(main_window, color[1], pygame.Rect((x+7)*pixel + width/2 + 1, y*pixel + height/2 + 1, pixel-2, pixel-2))"""
 
         #Game over
         if grille[0] != [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] and bloc_bundle[1][0] != 0:
@@ -159,13 +131,39 @@ while game_on:
                     main_window = pygame.display.set_mode()
     
     #Tout le temps actif
+    #Remplissage des couleurs dans la grille
+    #Variable pour centrer la grille
+    top_right_corner = [width/2 - 5*pixel, height/2 - 11*pixel]
+    if list(main_window.get_size())[0] == good_screen_resolution[0] and list(main_window.get_size())[1] == good_screen_resolution[1]:
+        pygame.draw.rect(main_window, color[0], pygame.Rect(top_right_corner[0] - int(1/2*pixel), top_right_corner[1] - int(1/2*pixel), 11*pixel, 23*pixel), int(1/2*pixel))
+    for y in range(len(grille)):
+        for x in range(len(grille[y])):
+            pygame.draw.rect(main_window, color[0], pygame.Rect(x*pixel + top_right_corner[0], y*pixel + top_right_corner[1], pixel, pixel), 1)
+            if grille[y][x] != 0:
+                pygame.draw.rect(main_window, color[grille[y][x]], pygame.Rect(x*pixel + top_right_corner[0]+1, y*pixel + top_right_corner[1]+1, pixel-2, pixel-2))
+                pygame.draw.rect(main_window, brighter_color[grille[y][x]], pygame.Rect(x*pixel + top_right_corner[0] + int(1/4*pixel), y*pixel + top_right_corner[1] + int(1/4*pixel), int(1/4*pixel), int(1/4*pixel)))
+    #Dessiner la pièce qui bouge
+    for y in range(len(bloc_bundle[0])):
+        for x in range(len(bloc_bundle[0][y])):
+            if bloc_bundle[0][y][x] != 0:
+                pygame.draw.rect(main_window, color[bloc_bundle[0][y][x]], pygame.Rect(x*pixel + top_right_corner[0]+1 + pixel*bloc_bundle[1][1], y*pixel + top_right_corner[1]+1 + pixel*bloc_bundle[1][0], pixel-2, pixel-2))
+                pygame.draw.rect(main_window, brighter_color[bloc_bundle[0][y][x]], pygame.Rect(x*pixel + top_right_corner[0] + int(1/4*pixel) + pixel*bloc_bundle[1][1], y*pixel + top_right_corner[1] + int(1/4*pixel) + pixel*bloc_bundle[1][0], int(1/4*pixel), int(1/4*pixel)))
+    #Dessiner la pièce qui va venir
+    for y in range(len(blocs[bloc_bundle[4][1][bloc_bundle[4][0]]])):
+        for x in range(len(blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y])):
+            if blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x] != 0:
+                pygame.draw.rect(main_window, color[0], pygame.Rect((x+7)*pixel + width/2, (y-7)*pixel + height/2, pixel, pixel), 2)
+                pygame.draw.rect(main_window, color[blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x]], pygame.Rect((x+7)*pixel + width/2 + 1, (y-7)*pixel + height/2 + 1, pixel-2, pixel-2))
+                pygame.draw.rect(main_window, brighter_color[blocs[bloc_bundle[4][1][bloc_bundle[4][0]]][y][x]], pygame.Rect((x+7)*pixel + width/2 + int(1/4*pixel), (y-7)*pixel + height/2 + int(1/4*pixel), int(1/4*pixel), int(1/4*pixel)))
+
+
     #Fonction de reset de la grille
     if keys[pygame.K_r]:
         grille, bloc_bundle, running, score_bundle = reset()
 
     #Afficher le titre au dessus
     police = pygame.font.Font("Brick Tetris.ttf", 2*pixel)
-    game_title =  police.render("TÉTRIS", True, color[4], color[1])
+    game_title =  police.render("TÉTRIS", True, color[8], color[1])
     game_titleRect = game_title.get_rect()
     game_titleRect.center = (width/2, height/2 - 13*pixel)
     main_window.blit(game_title, game_titleRect)
@@ -173,7 +171,7 @@ while game_on:
     #Afficher le score
     score_display =  police.render(str(score_bundle[2]), True, color[score_bundle[2]%9], color[1])
     score_displayRect = score_display.get_rect()
-    score_displayRect.topleft = (width/2 + 7_*pixel, height/2 - 11*pixel)
+    score_displayRect.topleft = (width/2 + 7*pixel, height/2 - 11*pixel)
     main_window.blit(score_display, score_displayRect)
 
     #Actualiser l'écran
